08c65aae402b2622f0fa8fb4770fe21ff79408be
conrad <connect@codezen.in>
2024-11-25 23:53:14 +0530

    changes-done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FoodMenuComponent } from './food-menu/food-menu.component';
+import { CustomerBillComponent } from './customer-bill/customer-bill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'menu', component: FoodMenuComponent},
+  {path: 'bill', component: CustomerBillComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavSectionComponent } from './sidenav-section/sidenav-section.component';
+import { MaterialModule } from './shared/material/material.module';
+import { FoodMenuComponent } from './food-menu/food-menu.component';
+import { CustomerBillComponent } from './customer-bill/customer-bill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavSectionComponent,
+    FoodMenuComponent,
+    CustomerBillComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
